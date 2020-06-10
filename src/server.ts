2f0fdeb642e89ed8c4863817ab446fca6ebcce16
Isaias <isaiasbatista96@gmail.com>
2020-06-10 16:01:01 -0300

    criando o projeto

---
+++ src/server.ts
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+app.get("/users", (request, response) => {
+  console.log("listagem de usuario");
+
+  response.json(["Isaias", "Batista", "França", "Silva"]);
+});
+
+app.post("/users", (request, response) => {
+  const user = {
+    name: "Isaias",
+    email: "isaiasbatista96@gmail.com",
+  };
+
+  return response.json(user);
+});
+
+app.listen(3333);
